571f596da63f4eb6bc682bd19fd435a8f7cbd6e6
Jack Tanner <jtanner0786@gmail.com>
2025-08-11 19:21:53 -0500

    dropdown and link

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,50 @@
 <template>
-    <div class="flex p-10 gap-x-5 bg-white h-screen">
-        <div>
-            <h1>Welcome to QR-Generator</h1>
-            <p>{{ hue }}</p>
-            <input v-model="hue" type="range" min="0" max="359" step="1" class="slider">
-            <br>
-            <p>{{ saturation }}</p>
-            <input v-model="saturation" type="range" min="0" max="100" step="1" class="slider">
-            <br>
-            <p>{{ lightness }}</p>
-            <input v-model="lightness" type="range" min="0" max="100" step="1" class="slider">
-        </div>
-        <div>
-            <!-- A higher "level" reduces chances of error if code is damaged -->
-            <QrcodeVue
-                :value="inputLink"
-                :foreground='textColor'
-                size="150"
-                render-as="canvas"
-                level='H' 
-            />
-        </div>
+  <div class="flex p-10 gap-x-5 bg-white h-screen">
+    <div>
+      <!--link of QR code-->
+      <label for="link" class="block text-gray-700 text-sm font-bold mb-2">QR Link:</label>
+      <input
+        type="text"
+        id="link"
+        v-model="qrLink"
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      />
+
+      <!--HSL values with sliders for values-->
+      <p>Hue: {{ hue }}</p>
+      <input v-model="hue" type="range" min="0" max="359" step="1" class="slider" />
+      <br />
+      <p>Saturation: {{ saturation }}</p>
+      <input v-model="saturation" type="range" min="0" max="100" step="1" class="slider" />
+      <br />
+      <p>Lightness: {{ lightness }}</p>
+      <input v-model="lightness" type="range" min="0" max="100" step="1" class="slider" />
+
+      <!--Choices for level of qr code-->
+      <Dropdown :items="['L', 'H', 'M', 'Q']" />
     </div>
-    
+    <div>
+      <!-- A higher "level" reduces chances of error if code is damaged -->
+      <!--Used to set values for QR code-->
+      <QrcodeVue :value="qrLink" :foreground="qrColor" size="150" render-as="canvas" level="" />
+    </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
 
 <script setup>
+import { ref, computed } from "vue";
+import QrcodeVue from "qrcode.vue";
+import Dropdown from "./components/Dropdown.vue";
 
-    import { ref, computed } from 'vue'
-    import QrcodeVue from 'qrcode.vue'
+const hue = ref(0);
+const saturation = ref(100);
+const lightness = ref(50);
+const qrLink = ref("");
+const errLevel = ref("");
 
-
-    const hue = ref(0)
-    const saturation = ref(100)
-    const lightness = ref(50)
-    const inputLink = ref('https://google.com')
-
-    const textColor = computed(() => {
-      return `hsla(${hue.value}, ${saturation.value}%, ${lightness.value}%, 100%)`
-    })
-
+const qrColor = computed(() => {
+  return `hsla(${hue.value}, ${saturation.value}%, ${lightness.value}%, 100%)`;
+});
 </script>
---
+++ frontend/src/components/Dropdown.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+});
+
+const emit = defineEmits({});
+</script>
+
+<template>
+  <div>
+    <select v-for="value in props.items">
+      <p>{{ value }}</p>
+    </select>
+  </div>
+</template>
